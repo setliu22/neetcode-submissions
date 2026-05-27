ae84f63cdabb7e946a51ae039697ec893d7acbe3
setliu22 <95052234+setliu22@users.noreply.github.com>
2026-05-27 12:43:45 -0400

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.py
@@ -0,0 +1,21 @@
+# Instead of using used, we use start
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        path = []
+
+        def backtrack(start):
+            res.append(path.copy())
+
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+
+                path.append(nums[i])
+                backtrack(i + 1)
+                path.pop()
+
+        backtrack(0)
+        return res
