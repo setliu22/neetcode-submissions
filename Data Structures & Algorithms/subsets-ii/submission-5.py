c2fc0de6ce98972c7b87f7089589881e5bbdf037
setliu22 <95052234+setliu22@users.noreply.github.com>
2026-05-27 12:54:50 -0400

    Add: subsets-ii - submission-5

---
+++ Data Structures & Algorithms/subsets-ii/submission-5.py
@@ -0,0 +1,27 @@
+# Instead of using used, we use start
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        path = []
+
+        # or you can do it like this
+        res.append([])
+
+        def backtrack(start):
+
+            for i in range(start, len(nums)):
+                # skip duplicates at same loop level, allow [1, 1]
+                # if i > start to make sure we can actually run the command
+                # we already chose this number at this decision level, don't overcount
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+
+                path.append(nums[i])
+                res.append(path.copy())
+                backtrack(i + 1)
+                path.pop()
+
+        backtrack(0)
+        return res
