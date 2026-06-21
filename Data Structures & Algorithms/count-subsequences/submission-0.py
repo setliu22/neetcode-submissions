0cb53551424898b73e3ef3816754f4e50429936a
setliu22 <95052234+setliu22@users.noreply.github.com>
2026-06-21 16:43:22 -0400

    Add: count-subsequences - submission-0

---
+++ Data Structures & Algorithms/count-subsequences/submission-0.py
@@ -0,0 +1,43 @@
+"""
+Let:
+
+dp[i][j]
+
+represent the number of ways to form the first j characters of t using the first i characters of s.
+
+For every character in s, there are two choices:
+
+Skip s[i - 1]
+Use s[i - 1] if it matches t[j - 1]
+
+base case: dp[i][0] = 1
+
+dp[i][j] = dp[i - 1][j]
+
+if characters match: dp[i][j] += dp[i - 1][j - 1] ( add that too )
+
+dp[2][1] counts the ways to get a c from the 2 characters
+
+dp[3][2] = dp[2][2] + dp[2][1]
+second is using the character
+
+only first dp[2][2] if no match
+"""
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(m + 1):
+            dp[i][0] = 1
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = dp[i - 1][j]
+
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] += dp[i - 1][j - 1]
+
+        return dp[m][n]
