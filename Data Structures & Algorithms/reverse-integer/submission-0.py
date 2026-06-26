58c43f7cf8d40e3bab77232aff4c61e8dc900be5
setliu22 <95052234+setliu22@users.noreply.github.com>
2026-06-26 19:19:48 -0400

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.py
@@ -0,0 +1,22 @@
+"""
+just do the easy way idk why this is in bit manipulation
+"""
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        result = 0
+
+        while x:
+            digit = x % 10
+            x //= 10
+            result = result * 10 + digit
+
+        result *= sign
+
+        if result < -(2**31) or result > 2**31 - 1:
+            return 0
+
+        return result
