071d1be64227806a879af8b9fa64610e4c78c7c1
setliu22 <95052234+setliu22@users.noreply.github.com>
2026-07-07 12:29:19 -0400

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.py
@@ -0,0 +1,34 @@
+"""
+min heap with dist
+there's only one start point
+"""
+
+import heapq
+from collections import defaultdict
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+
+        for u, v, w in times:
+            graph[u].append((v, w))
+
+        heap = [(0, k)]
+        dist = {}
+
+        while heap:
+            time, node = heapq.heappop(heap)
+
+            if node in dist:
+                continue
+
+            dist[node] = time
+
+            for nei, weight in graph[node]:
+                if nei not in dist:
+                    heapq.heappush(heap, (time + weight, nei))
+
+        if len(dist) != n:
+            return -1
+
+        return max(dist.values())
