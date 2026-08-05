06bac4c9baabb7357c74bd8b7d6279e5ae4ed167
setliu22 <95052234+setliu22@users.noreply.github.com>
2026-08-04 20:33:45 -0400

    Add: eating-bananas - submission-9

---
+++ Data Structures & Algorithms/eating-bananas/submission-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def solver(speed):
+            hours = 0
+            for pile in piles:
+                hours += (pile + speed - 1) // speed
+            return hours <= h
+
+        bot = 1
+        top = max(piles)
+
+        while bot < top:
+            middle = (bot + top) // 2
+
+            if solver(middle):
+                top = middle
+            else:
+                bot = middle + 1
+
+        return top
