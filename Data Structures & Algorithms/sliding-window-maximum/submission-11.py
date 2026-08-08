d81629a0f17415ad9984ea390b3c2928199c793b
setliu22 <95052234+setliu22@users.noreply.github.com>
2026-08-08 04:59:05 -0400

    Add: sliding-window-maximum - submission-11

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-11.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # when it gets in the window it destroys all the smaller stuff ahead of it
+        # a smaller added after it can still be the max
+        # give it a time or something so you know when to pop it from the top
+
+        ans = []
+
+        queue = deque()
+
+        for index, item in enumerate(nums):
+            if queue:
+                if queue[0][1] == index:
+                    queue.popleft()
+                while queue and queue[-1][0] <= item:
+                    queue.pop()
+                queue.append((item, index + k))
+
+                ans.append(queue[0][0])
+
+            else:
+                queue.append((item, index + k))
+                ans.append(item)   
+        
+        return ans[k-1:]
