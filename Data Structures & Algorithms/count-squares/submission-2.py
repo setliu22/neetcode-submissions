0d1cfb17e044d972e75e1fe4952b7e734dd41e1f
setliu22 <95052234+setliu22@users.noreply.github.com>
2026-08-12 14:40:22 -0400

    Add: count-squares - submission-2

---
+++ Data Structures & Algorithms/count-squares/submission-2.py
@@ -0,0 +1,51 @@
+class CountSquares:
+
+    def __init__(self):
+        self.grid = [[0] * 1001 for _ in range(1001)]
+
+    def add(self, point: List[int]) -> None:
+        self.grid[point[0]][point[1]] += 1
+
+    def count(self, point: List[int]) -> int:
+
+        x = point[0]
+        y = point[1]
+        
+        finalAns = 0
+        ans = 1
+
+        # try to find points to left, then find line above/below
+        for i in range(x - 1, -1, -1):
+            if self.grid[i][y] > 0:
+                ans *= self.grid[i][y]
+                
+                # find line above
+                for j in range(y - 1, -1, -1):
+                    if self.grid[i][j] > 0 and self.grid[x][j] > 0:
+                        finalAns += ans * self.grid[i][j] * self.grid[x][j]
+
+                # fine line below
+                for j in range(y + 1, 1000):
+                    if self.grid[i][j] > 0 and self.grid[x][j] > 0:
+                        finalAns += ans * self.grid[i][j] * self.grid[x][j]
+            
+            ans = 1
+
+        # try to find points to right, then find line above/below
+        for i in range(x + 1, 1000):
+            if self.grid[i][y] > 0:
+                ans *= self.grid[i][y]
+                
+                # find line above
+                for j in range(y - 1, -1, -1):
+                    if self.grid[i][j] > 0 and self.grid[x][j] > 0:
+                        finalAns += ans * self.grid[i][j] * self.grid[x][j]
+
+                # fine line below
+                for j in range(y + 1, 1000):
+                    if self.grid[i][j] > 0 and self.grid[x][j] > 0:
+                        finalAns += ans * self.grid[i][j] * self.grid[x][j]
+            
+            ans = 1
+        
+        return finalAns
