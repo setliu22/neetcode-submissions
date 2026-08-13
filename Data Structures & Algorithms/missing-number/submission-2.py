674bca6cb99c33ecf88a64c4a19a530222446d19
setliu22 <95052234+setliu22@users.noreply.github.com>
2026-08-13 09:11:10 -0400

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        ans = n
+
+        for i in range(n):
+            ans ^= i
+            ans ^= nums[i]
+    
+        return ans
